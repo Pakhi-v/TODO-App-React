66177117bd0d920ddbc53781ccfd6e7499354c0f
pakhi <pakhivashis182001@gmail.com>
2022-07-01 22:42:30 +0530

    modifying function app

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Do Yoga at evening', 'Go for a walk']);
+  const [input, setInput] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello everyone</h1>
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }

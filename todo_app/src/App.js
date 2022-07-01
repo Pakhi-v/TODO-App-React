394b77b8444de3186b4c925aec85a28c710cdd5c
pakhi <pakhivashis182001@gmail.com>
2022-07-01 22:58:55 +0530

    changes in console

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -4,12 +4,19 @@ import './App.css';
 function App() {
   const [todos, setTodos] = useState(['Do Yoga at evening', 'Go for a walk']);
   const [input, setInput] = useState('');
+  console.log('*',input);
+
+  const addTodo = (event) => {
+    // this will fire off when we click the button. 
+    console.log('+','Its working')
+    setTodos([...todos, input]);
+  }
 
   return (
     <div className="App">
       <h1>Hello everyone</h1>
-      <input />
-      <button>Add Todo</button>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add Todo</button>
 
       <ul>
         {todos.map(todo => (

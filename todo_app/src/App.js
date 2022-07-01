19b8f459631b4f7f11261e000efc9ecc8688e5dd
pakhi <pakhivashis182001@gmail.com>
2022-07-01 23:10:11 +0530

    adding setTodo

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -8,16 +8,20 @@ function App() {
 
   const addTodo = (event) => {
     // this will fire off when we click the button. 
+    event.preventDefault(); //will stop refreshing.
     console.log('+','Its working')
-    setTodos([...todos, input]);
+    setTodos([...todos, input]); //...todos indicate what previously is added
+    setInput(''); // clear the input after clicking on submit.
   }
 
   return (
     <div className="App">
       <h1>Hello everyone</h1>
+      <form>
       <input value={input} onChange={event => setInput(event.target.value)}/>
-      <button onClick={addTodo}>Add Todo</button>
-
+      <button type='submit' onClick={addTodo}>Add Todo</button>
+      </form>
+    
       <ul>
         {todos.map(todo => (
           <li>{todo}</li>

cd85b7664933a246aedfb86b3ec95d306fdfd906
pakhi <pakhivashis182001@gmail.com>
2022-07-04 21:14:54 +0530

    more updation in project

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,25 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button , FormControl, Input, InputLabel} from '@mui/material'
 import './App.css';
 import Todo from './Todo';
+import db from './firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 function App() {
-  const [todos, setTodos] = useState(['Do Yoga at evening', 'Go for a walk']);
+  const [todos, setTodos] = useState(['abc', 'def']);
   const [input, setInput] = useState('');
-  console.log('*',input);
+  
+  useEffect(() => {
+    // this code will fires when app.js loads
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => doc.data().todo))
+    })
+  }, []);
+  
 
   const addTodo = (event) => {
     // this will fire off when we click the button. 
     event.preventDefault(); //will stop refreshing.
-    console.log('+','Its working')
-    setTodos([...todos, input]); //...todos indicate what previously is added
+    
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
     setInput(''); // clear the input after clicking on submit.
-    console.log(todos);
   }
 
   return (
     <div className="App">
       <h1>Hello everyone</h1>
+      
       <form>
       {/*input value={input} onChange={event => setInput(event.target.value)}/> */}
 
--- todo_app/src/Todo.js
+++ todo_app/src/Todo.js
@@ -1,11 +1,12 @@
  import { List, ListItem, ListItemText } from '@mui/material'
+ import './Todo.css';
 import React from 'react'
  
  function Todo(props) {
    return (
      <List>
            <ListItem>
-            <ListItemText primary="Todo:" secondary={props.text} />
+            <ListItemText primary={props.text} secondary="Dummy deadline" />
            </ListItem>
            </List>
    )
